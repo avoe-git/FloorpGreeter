e866be8c5ebcf702af48b8dfba5ee3a758d801a1
avoe <inuser027@gmail.com>
2024-11-26 00:20:09 +0400

    v 1 ready

--- extended.js
+++ extended.js
@@ -1,7 +1,15 @@
-fetch('images.json')
-    .then(response => response.json())
-    .then(images => {
-        const randomImage = images[Math.floor(Math.random() * images.length)];
-        document.body.style.backgroundImage = `url('${randomImage}')`;
-    })
-    .catch(error => console.error('Error loading images:', error));
+fetch("images.json")
+  .then((response) => response.json())
+  .then((images) => {
+    const randomImage = images[Math.floor(Math.random() * images.length)];
+    document.body.style.backgroundImage = `url('${randomImage}')`;
+  })
+  .catch((error) => console.error("Error loading images:", error));
+function ChangeImage() {
+  fetch("images.json")
+    .then((response) => response.json())
+    .then((images) => {
+      const newImage = images[Math.floor(Math.random() * images.length)];
+      document.body.style.backgroundImage = `url('${newImage}')`;
+    });
+}
