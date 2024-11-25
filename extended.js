97b01f4baf4a4719a2eb39edbf0d73b74461c17f
avoe <inuser027@gmail.com>
2024-11-25 21:14:32 +0400

    branch reset

--- extended.js
+++
@@ -1,7 +0,0 @@
-fetch('images.json')
-    .then(response => response.json())
-    .then(images => {
-        const randomImage = images[Math.floor(Math.random() * images.length)];
-        document.body.style.backgroundImage = `url('${randomImage}')`;
-    })
-    .catch(error => console.error('Error loading images:', error));
