5633ea195ffe51e5a31756020f87e8993fb986f2
avoe <inuser027@gmail.com>
2024-12-30 04:46:21 +0400

    events added

--- extended.js
+++ extended.js
@@ -1,15 +1,28 @@
-fetch("images.json")
-  .then((response) => response.json())
-  .then((images) => {
-    const randomImage = images[Math.floor(Math.random() * images.length)];
-    document.body.style.backgroundImage = `url('${randomImage}')`;
-  })
-  .catch((error) => console.error("Error loading images:", error));
-function ChangeImage() {
+const eventNow = 'newyear';
+if (eventNow == 'default') {
   fetch("images.json")
+    .then((response) => response.json())
+    .then((images) => {
+      const randomImage = images[Math.floor(Math.random() * images.length)];
+      document.body.style.backgroundImage = `url('${randomImage}')`;
+    })
+    .catch((error) => console.error("Error loading images:", error));
+}
+else {
+  document.body.style.backgroundImage = `url('/Wallpapers/Events/wallpaper-${eventNow}.jpg')`;
+  document.getElementById("eventicon").src = `/Icons/event-${eventNow}.png`;
+  document.getElementById('eventButton').addEventListener("click", showEvent);
+  function showEvent() {
+    alert('Event: ' + eventNow);
+  };
+}
+function ChangeImage() {
+  if (eventNow == 'default') {
+    fetch("images.json")
     .then((response) => response.json())
     .then((images) => {
       const newImage = images[Math.floor(Math.random() * images.length)];
       document.body.style.backgroundImage = `url('${newImage}')`;
     });
+  }
 }
